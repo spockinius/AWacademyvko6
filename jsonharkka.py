e16ddd4ae821c09476da6dd6689085dc6197027c
spockinius <93668858+spockinius@users.noreply.github.com>
2022-03-04 13:33:04 +0200

    Delete jsonharkka.py

--- jsonharkka.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-tiedosto = requests.get("https://raw.githubusercontent.com/LearnWebCode/json-example/master/animals-1.json")
-
-data = tiedosto.json()
-
-for i in data:
-    if i["name"] == "Purrpaws":
-        print(i["name"], i["foods"]["likes"], i["foods"]["dislikes"])
